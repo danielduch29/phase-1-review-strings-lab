067f44818aaef9aeafc3e1a0ba9efa64f32a0154
Daniel <Daniel.duchatellier@gmail.com>
2021-04-05 08:39:17 -0400

    commit tree 970804a9155b49c6460266ec67cbcbc565cca979

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Daniel';
+/**const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!'; 
+ My template literal does not work**/
+ const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+ const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+ const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+ //It works now, I was not using backticks in the first error
